18b74daed78023b597e5ecb212728e6f2e28ad51
ngatuvac <ngatuvac5204@ad.faytechcc.edu>
2025-06-23 18:38:24 -1000

    Add files via upload
    First upload to Github P1LAB1

---
+++ P1LAB1_NgatuvaiCaleb.py
@@ -0,0 +1,12 @@
+#Caleb Ngatuvai
+#23 June 2025
+#P1LAB1
+#Welcome Input and Text for CTI-110
+
+print("Enter your first name:", end=" ")
+first_name = input()
+print("Enter your last name:", end="")
+last_name = input()
+print("Hello,", first_name, end=" ")
+print(last_name, end="!")
+print(" Welcome to CTI-110")
